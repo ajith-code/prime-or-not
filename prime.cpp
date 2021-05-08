0dc4f485f09cf84c95e27e8a57efb3cb49ce7988
ajith-code <70834458+ajith-code@users.noreply.github.com>
2021-05-08 11:43:27 +0530

    Add files via upload
    To check the given number is prime or not

---
+++ prime.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+    
+int a,i;
+    
+cout<<"Enter the number";
+    
+cin>>a;
+    
+if( a==1||a==0)
+    
+{
+        
+cout<<a<<"is not a prime number";
+    
+}
+    
+for( i=2;i<=a;i++)
+    
+{
+     
+if(a%i==0)
+     
+{
+        
+break; 
+     
+}
+    
+}
+    
+if(a==i)
+    
+{
+        
+cout<<a<<"is a prime number";
+        
+    
+}
+    
+else
+    
+{
+        
+cout<<a<<"is not a prime number";
+    
+}
+
+    
+return 0;
+
+}
+
